eeb9dde593cc857a2c02736c20d28c547216c1b1
rendrap <rendrapradana@gmail.com>
2021-03-07 08:02:51 +0700

    adding webpack config

---
+++ src/webpack.config.dev.js
@@ -0,0 +1,9 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+process.env.NODE_ENV = 'development';
+
+module.exports = {
+    mode: 'development'
+}
