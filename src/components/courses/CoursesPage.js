7103e3ca5204c8ef26ada9c6db0c8f9281885fae
rendrap <rendrapradana@gmail.com>
2021-03-08 15:49:52 +0700

    add courses page

---
+++ src/components/courses/CoursesPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class CoursesPage extends Component {
+  // state = {  }
+  render() {
+    return (
+      <h2>Courses</h2>
+    );
+  }
+}
+
+export default CoursesPage;
