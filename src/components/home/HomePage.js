bc495c5c3b8cd457f657a11ae7da18317fbb55e4
rendrap <rendrapradana@gmail.com>
2021-03-08 18:12:24 +0700

    update

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const Homepage = () => (
   <div className="jumbotron">
     <h1>Admin Page</h1>
-    <p>React, Redux Router</p>
+    <p>React {React.version} + Redux Router </p>
     <Link to="about" className="btn btn-primary btn-lg">
       Learn More
     </Link>
